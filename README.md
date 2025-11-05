451ac625b1144d270ff8f976975f793d8d28f8b4
mr-an <q@q.q>
2025-11-05 11:06:30 +0100

    intro added

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ This is a script on Python to plot the SQM (sky brightess in mag/arcsec^2) on ti
 2. Than run it and configure for plate solving. Press 'Show Stack Menu' button (with Sigma sign) or Ctrl+A, go to Alignment tab and fill in 'Field of view' (according to your images), 'Radius search area' (180*) and 'Star database used' (Auto). Refer to ASTASP documentation.
 3. Open your image (Ctrl+O or 'File-Load FITS or other format' menu or 'Browse & Preview FITS files' button) and try to run plate solving ('Solve' button). If it is Ok, try SQM measurement ('Tools-SQM report based on an image' menu). Fill in coordinates - Latitude and Longitude - of the shot place.
 4. Find the mean value of your Dark frames. You can press 'Show Stack Menu' button (with Sigma sign), go to 'Darks' tab and load your darks ('Browse' button). Than replace it with master dark by pressing 'Replace check-marked by one or more master dark'. You will find the mean value in this window. Use 'Analyse' button if needed.
-5. Substitute this mean value into the command in line ?? of the main.py file.
-6. Also change the default folder in line ??
-7. Run the script, choose files and wait until they are processed. Enjoy the SQM plot and data saved in *.spc text file.
+5. Substitute this mean value into the command in line 45 of the main.py file.
+6. Set the time zone used in the camera (for exif time).
+7. Also change the default folder in line 27.
+8Run the script, choose files and wait until they are processed. Enjoy the SQM plot and data saved in *.spc text file.
--- main.py
+++ main.py
@@ -9,12 +9,23 @@ from tkinter import filedialog
 import re
 
 def main():
+    print("""This is a script on Python to plot the SQM (sky brightess in mag/arcsec^2) on time using star photos and ASTAP program.
+
+1. First you should install ASTAP from https://www.hnsky.org/astap
+2. Than run it and configure for plate solving. Press 'Show Stack Menu' button (with Sigma sign) or Ctrl+A, go to Alignment tab and fill in 'Field of view' (according to your images), 'Radius search area' (180*) and 'Star database used' (Auto). Refer to ASTASP documentation.
+3. Open your image (Ctrl+O or 'File-Load FITS or other format' menu or 'Browse & Preview FITS files' button) and try to run plate solving ('Solve' button). If it is Ok, try SQM measurement ('Tools-SQM report based on an image' menu). Fill in coordinates - Latitude and Longitude - of the shot place.
+4. Find the mean value of your Dark frames. You can press 'Show Stack Menu' button (with Sigma sign), go to 'Darks' tab and load your darks ('Browse' button). Than replace it with master dark by pressing 'Replace check-marked by one or more master dark'. You will find the mean value in this window. Use 'Analyse' button if needed.
+5. Substitute this mean value into the command in line 45 of the main.py file.
+6. Set the time zone used in the camera (for exif time) in line 24. Camera (for exif local time) and PC time zone (for ASTAP) should be the same.
+7. Also change the default folder in line 27.
+8. Run the script, choose files and wait until they are processed. Enjoy the SQM plot and data saved in *.spc text file.
+""")
     # time zone for correcting the axis, because extracted is UTC time
     time_zone = np.timedelta64(3, 'h')
     # file open dialog window
     file_path = filedialog.askopenfilename(
         title="Выберите файл",
-        initialdir=r'C:\YandexDisk\Astronomy\2do\\',  # \\ to protect '
+        initialdir=r'C:\YandexDisk Mike\Astronomy\2do\\',  # \\ to protect '
         filetypes=[(['cr2', 'fit', 'xisf'], ['*.cr2', '*.fit', '*.xisf'])],  # to see which files are already processed
         multiple=True)
 
@@ -32,7 +43,8 @@ def main():
         # check if file already solved (is there a *.wcs file)
         if not os.path.exists(current_file_path[:-3] + 'wcs'):
             # Define the command and arguments for ASTAP
-            command = "\"C:\\Program Files\\astap\\astap.exe\" -f \"" + current_file_replaced + "\" -sqm 2046"
+            command = "\"C:\\Program Files\\astap\\astap.exe\" -f \"" + current_file_replaced + "\" -sqm 2046" # 6D
+            # command = "\"C:\\Program Files\\astap\\astap.exe\" -f \"" + current_file_replaced + "\" -sqm 1028.5" # 40D
             #print(command)
             # Run the command
             result = run(command, shell=True, capture_output=True, text=True)
